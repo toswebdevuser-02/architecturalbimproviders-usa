78346e595086f4e4658291650a7d084c7b7c3dec
toswebdevuser-02 <tos.webdevuser02@gmail.com>
2025-08-08 09:18:05 +0530

    Update layout-last.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,21 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         <link rel="canonical" href="https://architecturalbimproviders-usa.vercel.app /">
-        <script type="application/ld+json">
-          {"@context":"https://schema.org","@type":"Article","mainEntityOfPage":{"@type":"WebPage","@id":"https://architecturalbimproviders-usa.vercel.app/"},"headline":"Top 5 Architectural BIM Service Providers Reshaping American Design Requirements","description":"Discover the top 5 architectural BIM service providers transforming American design standards with cutting-edge technology, precision, and innovation in 2025.","image":"https://architecturalbimproviders-usa.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Farchitectural-bim-services.2c24078f.jpg&w=1200&q=75","datePublished":"2025-08-08","dateModified":"2025-08-08"}
-        </script>
+                  <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "Article",
+                "mainEntityOfPage": { "@type": "WebPage", "@id": "https://architecturalbimproviders-usa.vercel.app/" },
+                "headline": "Top 5 Architectural BIM Service Providers Reshaping American Design Requirements",
+                "description": "Discover the top 5 architectural BIM service providers transforming American design standards with cutting-edge technology, precision, and innovation in 2025.",
+                "image": "https://architecturalbimproviders-usa.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Farchitectural-bim-services.2c24078f.jpg&w=1200&q=75",
+                "datePublished": "2025-08-08",
+                "dateModified": "2025-08-08"
+              })
+            }}
+          />
         <meta name="google-site-verification" content="R5-G4Z02h_RxVS8TL7mvuj6GEC_mVch7EDWpk0UlGEc" />
       </head>
 
